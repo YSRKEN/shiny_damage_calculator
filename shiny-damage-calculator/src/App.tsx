618be01cfdf28f4bcc5cf4ef354cea0586d384e8
YSRKEN <ysr.ken@gmail.com>
2019-04-01 21:31:35 +0900

    React-Bootstrapを導入した

--- shiny-damage-calculator/src/App.tsx
+++ shiny-damage-calculator/src/App.tsx
@@ -1,8 +1,17 @@
 import * as React from 'react';
+import { Button, Col, Form, Row } from 'react-bootstrap';
 
 const App: React.FC = () => {
   return (
-    <h1>シャニマス火力計算機</h1>
+    <Row>
+      <Col className="mx-auto" xs={12} sm={10} md={8}>
+        <h1 className="text-center d-none d-sm-block my-3">シャニマス火力計算機</h1>
+        <h2 className="text-center d-xs-block d-sm-none my-3">シャニマス火力計算機</h2>
+        <Form className='text-center'>
+          <Button className='w-50 mx-auto'>テスト</Button>
+        </Form>
+      </Col>
+    </Row>
   );
 }
 
